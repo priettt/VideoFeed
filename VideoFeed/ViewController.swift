23d42b2f37c4aedf73f77fc2032ecc6460365788
Francisco Prieto <francisco.prieto@etermax.com>
2020-08-10 16:22:26 -0300

    Initial Commit

---
+++ VideoFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoFeed
+//
+//  Created by Francisco Prieto on 10/08/2020.
+//  Copyright © 2020 Francisco Cardelle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
